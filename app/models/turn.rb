919e43f7f2170c2dce43eacc414f45bc7aedb9f3
Alexandr Nesterenko <alexandr.nesterenko@softswiss.com>
2014-12-03 18:50:42 +0300

    history status

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -2,4 +2,15 @@ class Turn < ActiveRecord::Base
   belongs_to :table
   belongs_to :card_dealer, :class_name => 'Card'
   belongs_to :card_user, :class_name => 'Card'
+
+  #todo: replace the quick code
+  def get_history_class
+    if self.card_dealer.rate < self.card_user.rate
+      'history-win'
+    elsif self.card_dealer.rate > self.card_user.rate
+      'history-lose'
+    else
+      'history-draw'
+    end
+  end
 end
