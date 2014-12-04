44fe4c90747c1f8ec0a6005835f490593137fc62
Alexandr Nesterenko <alexandr.nesterenko@softswiss.com>
2014-12-04 11:02:46 +0300

    first tests

--- test/models/table_test.rb
+++ test/models/table_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TableTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user fast win" do
+    table = tables(:user_luck)
+
+    26.times.each {
+      table.turn
+    }
+    assert (table.finished and table.winner == Table::WINNER_USER), "User must win in 26 turns"
+  end
+
+  test "user not win in 25 turns" do
+    table = tables(:user_luck)
+
+    25.times.each {
+      table.turn
+    }
+    assert !table.finished?, "User must not win in 25 turns"
+  end
 end
